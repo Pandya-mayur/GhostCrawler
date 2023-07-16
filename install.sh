42a14c668eaf2ceb4ccc109aa9a704e2b25ebcda
Pandya Mayur <93024498+Pandya-mayur@users.noreply.github.com>
2023-07-16 13:38:17 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,12 +7,12 @@ mkdir -p tmp_dist
 pip install pyinstaller
 
 # Creates executable file and sends dependences to the recently created directories
-pyinstaller --onefile --workpath ./tmp_build --distpath ./tmp_dist --paths=src torbot/main.py
+pyinstaller --onefile --workpath ./tmp_build --distpath ./tmp_dist --paths=src GhostCrawler/main.py
 
 # Puts the executable in the current directory
-mv tmp_dist/torBot .
+mv tmp_dist/GhostCrawler .
 
 # Removes both directories and unneeded file
 rm -r tmp_build tmp_dist
-rm torBot.spec
+rm GhostCrawler.spec
 
